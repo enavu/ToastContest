cb82967b99c28ce836a12d6d6a9e2a74f8e394f0
Ena Phuong Vu <ena.phuong.n.vu@avanade.com>
2023-05-01 16:11:54 -0600

    push

--- ToastContest/MainPage.xaml.cs
+++ ToastContest/MainPage.xaml.cs
@@ -24,24 +24,48 @@ public partial class MainPage : ContentPage
 
     }
 
-	private void OnCounterClicked(object sender, EventArgs e)
+	private async void OnCounterClicked(object sender, EventArgs e)
 	{
-		count++;
 
-		if (count == 1)
-			CounterBtn.Text = $"Clicked {count} time";
-		else
-			CounterBtn.Text = $"Clicked {count} times";
+        CancellationTokenSource cancellationTokenSource = new CancellationTokenSource();
+        var snackbarOptions = new SnackbarOptions
+        {
+            BackgroundColor = Colors.Red,
+            TextColor = Colors.Green,
+            ActionButtonTextColor = Colors.Yellow,
+            CornerRadius = new CornerRadius(20),
+            Font = Microsoft.Maui.Font.SystemFontOfSize(14),
+            ActionButtonFont = Microsoft.Maui.Font.SystemFontOfSize(14),
+            CharacterSpacing = 0.5
 
-		SemanticScreenReader.Announce(CounterBtn.Text);
-	}
+        };
+
+        string text = "This is a Snackbar";
+        string actionButtonText = "Click Here to Dismiss";
+        TimeSpan duration = TimeSpan.FromSeconds(10);
+        Action action = async () => await App.Current.MainPage.DisplayAlert("Snackbar ActionButton Tapped", "The user has tapped the Snackbar ActionButton", "OK");
+
+        var snackbar = Snackbar.Make(text, action, actionButtonText, duration, new SnackbarOptions
+        {
+            BackgroundColor = Colors.Red,
+            TextColor = Colors.Green,
+            ActionButtonTextColor = Colors.Yellow,
+            CornerRadius = new CornerRadius(20),
+            Font = Microsoft.Maui.Font.SystemFontOfSize(14),
+            ActionButtonFont = Microsoft.Maui.Font.SystemFontOfSize(14),
+            CharacterSpacing = 0.5
+
+        }, TypeOfSnackBar);
+
+        await snackbar.Show(cancellationTokenSource.Token);
+    }
 
 	private void Slider_ValueChanged(object sender, ValueChangedEventArgs e)
 	{
 		sliderValue.Text = ((int)e.NewValue).ToString();
     }
 
-	public async void SetSnackBarSettings(object sender, ValueChangedEventArgs e)
+    private async void SetSnackBarSettings(object sender, ValueChangedEventArgs e)
 	{
         CancellationTokenSource cancellationTokenSource = new CancellationTokenSource();
         var snackbarOptions = new SnackbarOptions
