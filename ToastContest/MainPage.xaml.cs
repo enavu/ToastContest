7e27e414cf1af20364023417b8898e42400111dc
Ena Phuong Vu <ena.phuong.n.vu@avanade.com>
2023-05-10 12:40:24 -0600

    comments

--- ToastContest/MainPage.xaml.cs
+++ ToastContest/MainPage.xaml.cs
@@ -1,7 +1,5 @@
 ﻿using CommunityToolkit.Maui.Alerts;
 using CommunityToolkit.Maui.Core;
-using System.Numerics;
-using System.Threading;
 using ToastContest.ViewModel;
 
 namespace ToastContest;
@@ -11,6 +9,8 @@ public partial class MainPage : ContentPage
 	int count = 0;
     public ContentPage cp;
 	private readonly MainPageViewModel _vm;
+
+    //on load of the MainPage, the consturctor binds the view model and some data
 	public MainPage()
 	{
 		BindingContext = _vm = new MainPageViewModel();
@@ -23,19 +23,22 @@ public partial class MainPage : ContentPage
         List<string> TypeOfNotification = new List<string>() { "Success", "Warning" };
         TypeOfSnackBar.ItemsSource = TypeOfNotification;
 
-
     }
 
 	private async void OnCounterClicked(object sender, EventArgs e)
 	{
+        //Locations and Anchors for where/how the alert shows up.
         Picker vertical;
         Label horizontal;
         Color color;
         CancellationTokenSource cancellationTokenSource = new CancellationTokenSource();
+
+        //view model binding variables
         vertical = _vm.SelectedVertical == "Top" ? TypeOfSnackBar: VeritcalLocation;
         horizontal = _vm.SelectedHorizontal == "Left" ? Label1 : Label2;
         color = _vm.SelectedMessageType == "Success" ? Colors.Green : Colors.Red;
 
+        //Alert Snackbar Alert options
         var snackbarOptions = new SnackbarOptions
         {
             BackgroundColor = color,
@@ -44,19 +47,22 @@ public partial class MainPage : ContentPage
             CornerRadius = new CornerRadius(20),
             Font = Microsoft.Maui.Font.SystemFontOfSize(14),
             ActionButtonFont = Microsoft.Maui.Font.SystemFontOfSize(14),
-            CharacterSpacing = 0.5,
-            
-
-
+            CharacterSpacing = 0.5
         };
+
+        //Messages for alerts.
         string text = _vm.Message;
+
+        //Do things with the alert once pop up
         string actionButtonText = "Click Here to Dismiss";
         Int32 secs = Convert.ToInt32(Convert.ToDouble(_vm.Slider));
         TimeSpan duration = TimeSpan.FromSeconds(secs);
         Action action = async () => await App.Current.MainPage.DisplayAlert("Snackbar ActionButton Tapped", "The user has tapped the Snackbar ActionButton", "OK");
 
+        //Create Snackbar
         var snackbar = Snackbar.Make(text, action, actionButtonText, duration, snackbarOptions, vertical);
-
+        
+        //Show it
         await snackbar.Show(cancellationTokenSource.Token);
     }
 
--- ToastContest/ViewModel/MainPageViewModel.cs
+++ ToastContest/ViewModel/MainPageViewModel.cs
@@ -1,8 +1,4 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
-using CommunityToolkit.Mvvm.Input;
-using CommunityToolkit.Maui.Alerts;
-using CommunityToolkit.Maui.Core;
-using System.Threading;
 
 namespace ToastContest.ViewModel
 {
@@ -16,12 +12,16 @@ namespace ToastContest.ViewModel
         //View model used with Community Tool Kit compiled to general code that usually is seen to get and set
         [ObservableProperty]
         public string selectedVertical;
+
         [ObservableProperty]
         public string selectedHorizontal;
+
         [ObservableProperty]
         public string selectedMessageType;
+
         [ObservableProperty]
         public string message;
+
         [ObservableProperty]
         public string slider;
     }
